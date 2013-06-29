1d9d4d85b0865f19fa5c9f46f5536a56e0e9728b
Ubuntu_Ruby <user@ubuntu.(none)>
2013-06-29 10:08:58 +0300

    twitt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NevgeniyTwitter::Application.config.secret_token = 'c959a0f629a028cd50b26148016a3a93e65e45f0e1cff84cb62080235673940fb6dbdd09e169cf18a1fefb5efbe3966292584c7c659b07c46ef78d76854050d2'
